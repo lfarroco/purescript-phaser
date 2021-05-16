19a0d53a064469f4fd91c8cd5a795859f1390c19
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-05-15 22:21:57 -0300

    adds comment to delay

--- src/Graphics/Time.purs
+++ src/Graphics/Time.purs
@@ -1,12 +1,18 @@
 module Graphics.Phaser.Time where
 
 import Prelude
-
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 import Phaser.Graphics.ForeignTypes (PhaserScene)
 
 foreign import delay_ :: PhaserScene -> Int -> EffectFnAff Unit
 
+-- | Example usage: Destroy a container after 1s
+-- | destroyContainer :: PhaserScene -> PhaserContainer -> Effect Unit
+-- | destroyContainer scene cont =
+-- |   launchAff_ do
+-- |     delay scene 1000
+-- |     liftEffect $ do
+-- |         destroy cont
 delay :: PhaserScene -> Int -> Aff Unit
 delay a = fromEffectFnAff <<< delay_ a
