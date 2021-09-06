575fa912c58a35df6211741fbd144795f8543ae3
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-09-06 00:52:02 -0300

    text - add more bindings

--- src/Graphics/Text.js
+++ src/Graphics/Text.js
@@ -1,5 +1,101 @@
 'use strict';
 
-exports.createImpl = function ({ pos, text, config }, scene) {
-  return scene.add.text(pos.x, pos.y, text, config);
+exports.createImpl = function (text, scene) {
+  return scene.add.text(0, 0, text);
+};
+
+exports.setTextImpl = function (value, text) {
+  text.setText(value);
+  return text;
+};
+
+exports.setStyleImpl = function (value, text) {
+  text.setStyle(value);
+  return text;
+};
+
+exports.setColorImpl = function (value, text) {
+  text.setColor(value);
+  return text;
+};
+
+exports.setFontImpl = function (value, text) {
+  text.setFont(value);
+  return text;
+};
+
+exports.setFontFamilyImpl = function (value, text) {
+  text.setFontFamily(value);
+  return text;
+};
+
+exports.setFontSizeImpl = function (value, text) {
+  text.setFontSize(value);
+  return text;
+};
+
+exports.setFontStyleImpl = function (value, text) {
+  text.setFontStyle(value);
+  return text;
+};
+
+exports.setLineSpacingImpl = function (value, text) {
+  text.setLineSpacing(value);
+  return text;
+};
+
+exports.setMaxLinesImpl = function (value, text) {
+  text.setMaxLines(value);
+  return text;
+};
+
+exports.setPaddingImpl = function ({ left, top, right, bottom }, text) {
+  text.setPadding(left, top, right, bottom);
+  return text;
+};
+
+exports.setShadowImpl = function (text) {
+  text.setShadow();
+  return text;
+};
+
+exports.setShadowBlurImpl = function (value, text) {
+  text.setShadowBlur(value);
+  return text;
+};
+
+exports.setShadowColorImpl = function (value, text) {
+  text.setShadowColor(value);
+  return text;
+};
+
+exports.setShadowFillImpl = function (value, text) {
+  text.setShadowFill(value);
+  return text;
+};
+
+exports.setShadowOffsetImpl = function ({ x, y }, text) {
+  text.setShadowOffset(x, y);
+  return text;
+};
+exports.setShadowStrokeImpl = function (value, text) {
+  text.setShadowStroke(value);
+  return text;
+};
+exports.setStrokeImpl = function (value, text) {
+  text.setStroke(value);
+  return text;
+};
+
+exports.setShadowStrokeImpl = function (value, text) {
+  text.setShadowStroke(value);
+  return text;
+};
+exports.updateTextImpl = function (text) {
+  text.updateText();
+  return text;
+};
+
+exports.getTextMetrics = function (text) {
+  return text.getTextMetrics();
 };
--- src/Graphics/Text.purs
+++ src/Graphics/Text.purs
@@ -1,16 +1,143 @@
-module Graphics.Phaser.Text (create, TextConfig) where
+module Graphics.Phaser.Text
+  ( create
+  , setText
+  , setColor
+  , setFontFamily
+  , setFont
+  , setFontSize
+  , setFontStyle
+  , setLineSpacing
+  , setMaxLines
+  , setPadding
+  , setShadowBlur
+  , setShadowColor
+  , setShadowFill
+  , setShadow
+  , setShadowOffset
+  , setShadowStroke
+  , setStroke
+  , setStyle
+  , updateText
+  ) where
 
-import Effect.Uncurried (EffectFn2, runEffectFn2)
+-- https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Text.html
+import Prelude
 import Effect (Effect)
+import Effect.Uncurried (EffectFn2, EffectFn3, runEffectFn2, runEffectFn3)
 import Graphics.Phaser.ForeignTypes (PhaserScene, PhaserText)
+import Graphics.Phaser.GameObject (Vector)
 
-type TextConfig
-  = { pos :: { x :: Number, y :: Number }
-    , text :: String
-    , config :: { color :: String, fontSize :: Int, fontFamily :: String }
+type SingleValueEffect a
+  = EffectFn2 a PhaserText PhaserText
+
+foreign import createImpl :: EffectFn2 String PhaserScene PhaserText
+
+create :: String -> PhaserScene -> Effect PhaserText
+create = runEffectFn2 createImpl
+
+foreign import setTextImpl :: SingleValueEffect String
+
+setText :: String -> PhaserText -> Effect PhaserText
+setText = runEffectFn2 setTextImpl
+
+type PhaserTextMetrics
+  = { ascent :: Number, descent :: Number, fontSize :: Number }
+
+foreign import getTextMetrics :: PhaserText -> PhaserTextMetrics
+
+foreign import setColorImpl :: SingleValueEffect String
+
+setColor :: String -> PhaserText -> Effect PhaserText
+setColor = runEffectFn2 setColorImpl
+
+foreign import setFontFamilyImpl :: SingleValueEffect String
+
+setFontFamily :: String -> PhaserText -> Effect PhaserText
+setFontFamily = runEffectFn2 setFontFamilyImpl
+
+foreign import setFontImpl :: SingleValueEffect String
+
+setFont :: String -> PhaserText -> Effect PhaserText
+setFont = runEffectFn2 setFontImpl
+
+foreign import setFontSizeImpl :: SingleValueEffect Number
+
+setFontSize :: Number -> PhaserText -> Effect PhaserText
+setFontSize = runEffectFn2 setFontSizeImpl
+
+foreign import setFontStyleImpl :: SingleValueEffect String
+
+setFontStyle :: String -> PhaserText -> Effect PhaserText
+setFontStyle = runEffectFn2 setFontStyleImpl
+
+foreign import setLineSpacingImpl :: SingleValueEffect Number
+
+setLineSpacing :: Number -> PhaserText -> Effect PhaserText
+setLineSpacing = runEffectFn2 setLineSpacingImpl
+
+foreign import setMaxLinesImpl :: SingleValueEffect Number
+
+setMaxLines :: Number -> PhaserText -> Effect PhaserText
+setMaxLines = runEffectFn2 setMaxLinesImpl
+
+type SetPaddingConfig
+  = { left :: Number, top :: Number, right :: Number, bottom :: Number }
+
+foreign import setPaddingImpl :: SingleValueEffect SetPaddingConfig
+
+setPadding :: SetPaddingConfig -> PhaserText -> Effect PhaserText
+setPadding = runEffectFn2 setPaddingImpl
+
+foreign import setShadowBlurImpl :: SingleValueEffect Number
+
+setShadowBlur :: Number -> PhaserText -> Effect PhaserText
+setShadowBlur = runEffectFn2 setShadowBlurImpl
+
+foreign import setShadowColorImpl :: SingleValueEffect String
+
+setShadowColor :: String -> PhaserText -> Effect PhaserText
+setShadowColor = runEffectFn2 setShadowColorImpl
+
+foreign import setShadowFillImpl :: SingleValueEffect String
+
+setShadowFill :: String -> PhaserText -> Effect PhaserText
+setShadowFill = runEffectFn2 setShadowFillImpl
+
+foreign import setShadowImpl :: SingleValueEffect Unit
+
+setShadow :: Unit -> PhaserText -> Effect PhaserText
+setShadow = runEffectFn2 setShadowImpl
+
+foreign import setShadowOffsetImpl :: SingleValueEffect Vector
+
+setShadowOffset :: Vector -> PhaserText -> Effect PhaserText
+setShadowOffset = runEffectFn2 setShadowOffsetImpl
+
+foreign import setShadowStrokeImpl :: SingleValueEffect Boolean
+
+setShadowStroke :: Boolean -> PhaserText -> Effect PhaserText
+setShadowStroke = runEffectFn2 setShadowStrokeImpl
+
+foreign import setStrokeImpl :: EffectFn3 String Number PhaserText PhaserText
+
+-- | Color, thickness, text object
+setStroke :: String -> Number -> PhaserText -> Effect PhaserText
+setStroke = runEffectFn3 setStrokeImpl
+
+type SetStyleConfig
+  = { fontSize :: String
+    , fontFamily :: String
+    , color :: String
+    , align :: String
+    , backgroundColor :: String
     }
 
-foreign import createImpl :: EffectFn2 TextConfig PhaserScene PhaserText
+foreign import setStyleImpl :: SingleValueEffect SetStyleConfig
 
-create :: TextConfig -> PhaserScene -> Effect PhaserText
-create = runEffectFn2 createImpl
+setStyle :: SetStyleConfig -> PhaserText -> Effect PhaserText
+setStyle = runEffectFn2 setStyleImpl
+
+foreign import updateTextImpl :: SingleValueEffect Unit
+
+updateText :: Unit -> PhaserText -> Effect PhaserText
+updateText = runEffectFn2 updateTextImpl
