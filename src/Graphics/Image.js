4b0a028d6da7a4570937b06347bc7460e89e9a6a
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-09-05 23:36:44 -0300

    image - remove getscene (use from GO), simplify create

--- src/Graphics/Image.js
+++ src/Graphics/Image.js
@@ -1,8 +1,3 @@
-"use strict";
+'use strict';
 
-exports.createImpl = (textureKey, { x, y }, scene) =>
-  scene.add.image(x, y, textureKey);
-
-exports.getSceneImpl = (img) =>
-  img.scene;
-
+exports.createImpl = (textureKey, scene) => scene.add.image(0, 0, textureKey);
--- src/Graphics/Image.purs
+++ src/Graphics/Image.purs
@@ -1,30 +1,11 @@
-module Graphics.Phaser.Image (create, getScene) where
+module Graphics.Phaser.Image (create) where
 
 import Effect (Effect)
-import Effect.Uncurried
-  ( EffectFn1
-  , EffectFn3
-  , runEffectFn1
-  , runEffectFn3
-  )
+import Effect.Uncurried (EffectFn2, runEffectFn2)
 import Graphics.Phaser.ForeignTypes (PhaserImage, PhaserScene)
 
 -- | Create a new image in the given scene
-foreign import createImpl ::
-  EffectFn3
-    String
-    { x :: Number, y :: Number }
-    PhaserScene
-    PhaserImage
+foreign import createImpl :: EffectFn2 String PhaserScene PhaserImage
 
-create ::
-  String ->
-  { x :: Number, y :: Number } ->
-  PhaserScene ->
-  Effect PhaserImage
-create = runEffectFn3 createImpl
-
-foreign import getSceneImpl :: EffectFn1 PhaserImage PhaserScene
-
-getScene :: PhaserImage -> Effect PhaserScene
-getScene = runEffectFn1 getSceneImpl
+create :: String -> PhaserScene -> Effect PhaserImage
+create = runEffectFn2 createImpl
