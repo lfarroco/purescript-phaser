e3aa68ee8da50aff1090cb326737dd0a075ebebb
Leonardo Farroco <leonardo.farroco@gmail.com>
2022-03-06 21:48:19 -0300

    container

--- src/Graphics/Image.purs
+++ src/Graphics/Image.purs
@@ -2,7 +2,9 @@ module Graphics.Phaser.Image (create) where
 
 import Effect (Effect)
 import Graphics.Phaser.ForeignTypes (PhaserImage, PhaserScene)
+import Graphics.Phaser.GameObject (class GameObject)
 import Utils.FFI (return1)
+
 
 create :: String -> PhaserScene -> Effect PhaserImage
 create = return1 "add.image(0, 0, v1)"
