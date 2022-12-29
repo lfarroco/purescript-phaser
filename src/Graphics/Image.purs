f49a18b6224b5532a8229e4847d69ee41f0e023b
Leonardo Farroco <leonardo.farroco@gmail.com>
2022-12-29 16:38:14 -0300

    add setTexture

--- src/Graphics/Image.purs
+++ src/Graphics/Image.purs
@@ -1,9 +1,12 @@
-module Graphics.Phaser.Image (create) where
+module Graphics.Phaser.Image where
 
 import Prelude
 import Effect (Effect)
 import Graphics.Phaser.ForeignTypes (PhaserImage, PhaserScene)
-import Utils.FFI (_getProp, _return3)
+import Utils.FFI (_getProp, _return3, _method1)
 
 create :: String -> PhaserScene -> Effect PhaserImage
 create v1 = _getProp "add" >=> _return3 "image" 0 0 v1
+
+setTexture :: String -> PhaserImage -> Effect PhaserImage
+setTexture = _method1 "setTexture"
