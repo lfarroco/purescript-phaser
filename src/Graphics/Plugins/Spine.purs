629576554fa55f4786a97fa81282ce2bc96b4914
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-03-13 13:38:46 -0300

    spine: add setSkinByName

--- src/Graphics/Plugins/Spine.purs
+++ src/Graphics/Plugins/Spine.purs
@@ -2,12 +2,12 @@ module Graphics.Phaser.Plugins.Spine where
 
 import Prelude
 import Effect (Effect)
-import Graphics.Phaser.ForeignTypes (PhaserScene, PhaserSpineGameObject)
 import Graphics.Phaser.CoreTypes (Vector)
-import Utils.FFI(_getProp, _method3, _return5 , _return4)
+import Graphics.Phaser.ForeignTypes (PhaserScene, PhaserSpineGameObject)
+import Utils.FFI (_getProp, _method1, _method3, _return4, _return5)
 
 loadAssetFromScene ::
-  String -> 
+  String ->
   String ->
   Array String ->
   Boolean ->
@@ -15,13 +15,16 @@ loadAssetFromScene ::
   Effect PhaserScene
 loadAssetFromScene key path atlases preMultipliedAlpha scn =
   _getProp "load" scn
-    >>= _return4 "spine" key path atlases preMultipliedAlpha 
+    >>= _return4 "spine" key path atlases preMultipliedAlpha
     >>= const (pure scn)
 
-createSkeletonFromScene :: { position :: Vector, skeletonKey :: String, animation :: String, preMultipliedAlpha :: Boolean  } -> PhaserScene  -> Effect PhaserSpineGameObject
-createSkeletonFromScene {position, skeletonKey, animation, preMultipliedAlpha} = 
-  _getProp "add" 
+createSkeletonFromScene :: { position :: Vector, skeletonKey :: String, animation :: String, preMultipliedAlpha :: Boolean } -> PhaserScene -> Effect PhaserSpineGameObject
+createSkeletonFromScene { position, skeletonKey, animation, preMultipliedAlpha } =
+  _getProp "add"
     >=> _return5 "spine" position.x position.y skeletonKey animation preMultipliedAlpha
 
 play :: String -> Boolean -> Boolean -> PhaserSpineGameObject -> Effect PhaserSpineGameObject
 play = _method3 "play"
+
+setSkinByName :: String -> PhaserSpineGameObject -> Effect PhaserSpineGameObject
+setSkinByName = _method1 "setSkinByName"
